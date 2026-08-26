fb09bcc73d8f9c187392504622cf8666053320fa
fathurrahmanrifaldi <fathurrahmanrifaldi@gmail.com>
2026-08-26 17:23:06 +0700

    new trainer img

--- src/data/trainers.ts
+++ src/data/trainers.ts
@@ -8,20 +8,20 @@ export interface Trainer {
 
 export const trainers: Trainer[] = [
   {
-    name: "Coach Putra",
-    role: "Certified Personal Trainer",
-    specializations: ["Muscle Gain", "Fat Loss", "Body Recomposition"],
-    description:
-      "Membantu kamu menyusun dan menjalankan program latihan sesuai dengan tujuan pembentukan tubuh dan kebugaran.",
-    image: "/images/trainer-putra.jpg", // TODO: ganti dengan foto asli trainer
-  },
-  {
     name: "Coach Ridwan",
     role: "Certified Personal Trainer",
     specializations: ["Muscle Gain", "Fat Loss", "Body Recomposition"],
     description:
+      "Membantu kamu menyusun dan menjalankan program latihan sesuai dengan tujuan pembentukan tubuh dan kebugaran.",
+    image: "/images/coach_ridwan.png",
+  },
+  {
+    name: "Coach Putra",
+    role: "Certified Personal Trainer",
+    specializations: ["Muscle Gain", "Fat Loss", "Body Recomposition"],
+    description:
       "Mendampingi proses latihan untuk membantu meningkatkan massa otot, mengurangi lemak tubuh, dan mencapai komposisi tubuh yang lebih ideal.",
-    image: "/images/trainer-ridwan.jpg", // TODO: ganti dengan foto asli trainer
+    image: "/images/coach_putra.png",
   },
   {
     name: "Coach Adhe",
@@ -29,6 +29,6 @@ export const trainers: Trainer[] = [
     specializations: ["Muscle Gain", "Fat Loss", "Body Recomposition"],
     description:
       "Membantu mengarahkan latihan secara terstruktur agar kamu dapat berlatih lebih efektif dan konsisten menuju targetmu.",
-    image: "/images/trainer-adhe.jpg", // TODO: ganti dengan foto asli trainer
+    image: "/images/coach_adhe.png",
   },
 ];
